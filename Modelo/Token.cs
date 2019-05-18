2cdffcd598eaf00daf2ef4c895c1057ed762c46e
Augusto <augustohtp8@gmail.com>
2019-05-18 00:37:02 -0300

    API+ Jwt [.NetFramework]

---
+++ Modelo/Token.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Token
+    {
+        public Token()
+        {
+            Expiress = 5;
+            Issuer = "Emprestei";
+            Audience = "Emprestei";
+            SecurityKey = "oj11cy5k5sznune7yico2pfwtupt7xyl6od9d9k8r9mdmrbb0d";
+        }
+        public string SecurityKey { get; set; }
+        public int Expiress { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
